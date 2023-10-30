2011708bc56b72c1953da7bcb0354074640445fc
Vanshika Dhingra <vanshika.dhingra@students.iiit.ac.in>
2023-10-30 18:29:10 +0530

    added graph

--- analysis/pilotAnalysis.py
+++ analysis/pilotAnalysis.py
@@ -1,12 +1,12 @@
 import csv
 import re
-
+import copy 
 # Define a regular expression pattern to match IDs starting with '#'
 pattern = r'#\w+\d'
 
 # Initialize a dictionary to store counts for each ID (question ID) and figure
 id_figure_counts = {}
-
+user =[]
 # Open the CSV file
 with open('Aesthetics Of Simple Shapes.csv', 'r') as csvfile:
     reader = csv.reader(csvfile)
@@ -26,7 +26,26 @@ with open('Aesthetics Of Simple Shapes.csv', 'r') as csvfile:
                 id_figure_counts[id][row[6+i]]=1
             else:
                 id_figure_counts[id][row[6+i]]+=1
+            user.append(copy.deepcopy(id_figure_counts[id]))
 
-#print the counts
-for id in id_figure_counts:
-    print(id, id_figure_counts[id])
+count=[0,0,0,0,0,0,0]
+for id in ids:
+    for i in range(1,7):
+        if 'Figure - '+str(i)+'' not in id_figure_counts[id]:
+            count[i]=count[i]
+        else:
+            count[i]+=id_figure_counts[id]['Figure - '+str(i)+'']
+
+#plot a bar graph for count
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.arange(1,7)
+y = count[1:7]
+plt.bar(x, y, color='green')
+plt.xlabel('Figure Number')
+plt.ylabel('Count')
+plt.title('Count of Figure Number')
+plt.show()
+
+
+
